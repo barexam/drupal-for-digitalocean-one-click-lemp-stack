30ecfd2bf0f18d54a13bd026a9cd4d8d45d1e9b6
James Damian Hakert <jdhakert@me.com>
2018-09-14 01:02:51 -0400

    Updated spacing

--- install-drupal-8.sh
+++ install-drupal-8.sh
@@ -29,7 +29,7 @@ server {
         access_log off;
     }
 
-    # Very rarely should these ever be accessed outside of your lan
+    ## Very rarely should these ever be accessed outside of your lan
     location ~* \.(txt|log)$ {
         allow 192.168.0.0/16;
         deny all;
@@ -43,14 +43,14 @@ server {
         return 403;
     }
 
-    # Allow "Well-Known URIs" as per RFC 5785
+    ## Allow "Well-Known URIs" as per RFC 5785
     location ~* ^/.well-known/ {
         allow all;
     }
 
-    # Block access to "hidden" files and directories whose names begin with a
-    # period. This includes directories used by version control systems such
-    # as Subversion or Git to store control files.
+    ## Block access to "hidden" files and directories whose names begin with a
+    ## period. This includes directories used by version control systems such
+    ## as Subversion or Git to store control files.
     location ~ (^|/)\. {
         return 403;
     }
@@ -64,50 +64,50 @@ server {
         rewrite ^/(.*)$ /index.php?q=$1;
     }
 
-    # Don't allow direct access to PHP files in the vendor directory.
+    ## Don't allow direct access to PHP files in the vendor directory.
     location ~ /vendor/.*\.php$ {
         deny all;
         return 404;
     }
 
-    # In Drupal 8, we must also match new paths where the '.php' appears in
-    # the middle, such as update.php/selection. The rule we use is strict,
-    # and only allows this pattern with the update.php front controller.
-    # This allows legacy path aliases in the form of
-    # blog/index.php/legacy-path to continue to route to Drupal nodes. If
-    # you do not have any paths like that, then you might prefer to use a
-    # laxer rule, such as:
-    #   location ~ \.php(/|$) {
-    # The laxer rule will continue to work if Drupal uses this new URL
-    # pattern with front controllers other than update.php in a future
-    # release.
+    ## In Drupal 8, we must also match new paths where the '.php' appears in
+    ## the middle, such as update.php/selection. The rule we use is strict,
+    ## and only allows this pattern with the update.php front controller.
+    ## This allows legacy path aliases in the form of
+    ## blog/index.php/legacy-path to continue to route to Drupal nodes. If
+    ## you do not have any paths like that, then you might prefer to use a
+    ## laxer rule, such as:
+    ##   location ~ \.php(/|$) {
+    ## The laxer rule will continue to work if Drupal uses this new URL
+    ## pattern with front controllers other than update.php in a future
+    ## release.
     location ~ '\.php$|^/update.php' {
         fastcgi_split_path_info ^(.+?\.php)(|/.*)$;
-        # Security note: If you're running a version of PHP older than
-        # 5.3, you should have "cgi.fix_pathinfo = 0;" in php.ini.
-        # See http://serverfault.com/q/627903/94922 for details.
+        ## Security note: If you're running a version of PHP older than
+        ## 5.3, you should have "cgi.fix_pathinfo = 0;" in php.ini.
+        ## See http://serverfault.com/q/627903/94922 for details.
         include fastcgi_params;
-        # Block httpoxy attacks. See https://httpoxy.org/.
+        ## Block httpoxy attacks. See https://httpoxy.org/.
         fastcgi_param HTTP_PROXY "";
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_param PATH_INFO $fastcgi_path_info;
         fastcgi_param QUERY_STRING $query_string;
         fastcgi_intercept_errors on;
-        # PHP 5 socket location.
+        ## PHP 5 socket location.
         #fastcgi_pass unix:/var/run/php5-fpm.sock;
-        # PHP 7 socket location.
+        ## PHP 7 socket location.
         fastcgi_pass 127.0.0.1:9000;
     }
 
-    # Fighting with Styles? This little gem is amazing.
-    # location ~ ^/sites/.*/files/imagecache/ { # For Drupal <= 6
-    location ~ ^/sites/.*/files/styles/ { # For Drupal >= 7
+    ## Fighting with Styles? This little gem is amazing.
+    ## location ~ ^/sites/.*/files/imagecache/ { ## For Drupal <= 6
+    location ~ ^/sites/.*/files/styles/ { ## For Drupal >= 7
         try_files $uri @rewrite;
     }
 
-    # Handle private files through Drupal. Private file's path can come
-    # with a language prefix.
-    location ~ ^(/[a-z\-]+)?/system/files/ { # For Drupal >= 7
+    ## Handle private files through Drupal. Private file's path can come
+    ## with a language prefix.
+    location ~ ^(/[a-z\-]+)?/system/files/ { ## For Drupal >= 7
         try_files $uri /index.php?$query_string;
     }
 
@@ -117,7 +117,7 @@ server {
         log_not_found off;
     }
 
-    # phpMyAdmin Config
+    ## phpMyAdmin Config
     location /phpmyadmin {
            root /usr/share/;
            index index.php index.html index.htm;
@@ -151,13 +151,13 @@ sudo chown -R :www-data /var/www/html/drupal/*
 chmod -R 777  /var/www/html/drupal/sites/default/files
 chmod 664 /var/www/html/drupal/sites/default/settings.php
 
-# Install phpMyAdmin
+## Install phpMyAdmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-all-languages.zip
 sudo apt-get install unzip
 unzip phpMyAdmin-4.8.2-all-languages.zip
 mv phpMyAdmin-4.8.2-all-languages /usr/share/phpmyadmin
 
-# Install Composer & Drush
+## Install Composer & Drush
 sudo apt-get install php7.2-mbstring php7.2-curl
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 wget -O drush.phar https://github.com/drush-ops/drush-launcher/releases/download/0.6.0/drush.phar
